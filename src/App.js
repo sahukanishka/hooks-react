d12f1311683d67315f678dbd658b9957e2210706
sahukanishka <kanishkasahu005@gmail.com>
2021-03-05 14:51:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Todo({todo,index,completeTodo}) {
+  return(
+    <div>
+      <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo-row">{todo.text}
+      <button onClick={() => completeTodo(index)} >Complete</button>
+      </div>
+      </div>
+     
+  
+  )
 }
 
-export default App;
+
+
+function TodoForm({addTodo}) { 
+  const [value,setValue] = useState("");
+
+  const handleSubmit = (event) => {
+  event.preventDefault();
+  if(!value) return ; 
+  addTodo(value);
+  setValue("");
+}
+
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <p className="input-text">Just Add your task and press enter</p>
+      <input placeholder="Enter your todo task" type="text" className="input-box" value={value} onChange={event => setValue(event.target.value)} />
+    </form>
+  )
+}
+
+
+function App() {
+  const [todos,setTodos] = useState([
+    {
+      text : "new one 1",
+      isCompleted : false 
+    },
+    {
+      text : "new one 3",
+      isCompleted : false 
+    },
+    {
+      text : "new one 2",
+      isCompleted : false 
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos,{text}];
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true ;
+    setTodos(newTodos)
+  }
+
+
+  return (
+    <div className="App">
+      <h3 className="main-heading">Todos App with Hooks </h3>
+      <div className="todo-card">
+        { todos.map((todo,index) => (
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo}/>
+      </div>
+    </div>
+  )
+}
+
+export default App ; 
